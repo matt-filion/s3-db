0bebc2904158886fa5dd13d85ad24eda3d1dd140
matt_filion <matt_filion@localhost>
2016-08-21 16:38:07 -0700

    Few manual tests to help ensure common cases are still working. Precursor to proper automated testing.

---
+++ test/test-general.js
@@ -0,0 +1,57 @@
+const s3db = require('../src')({
+  accessKeyId  : '',
+  secretAccessKey : ''
+//  ,errorOnNotFound:true
+});
+
+var users = s3db.bucketOf('users');
+
+/*
+ * List out users
+ */
+//users
+//  .list()
+//  .then( (results) => {console.log('results',results)});
+
+/*
+ * Load a record that doesnt exist.
+ */
+
+//users
+//  .load("!@#kljasdpf123jqw-d09fj")
+//  .then(user => console.log("user --> ",user))
+//  .fail( error => console.error("error",error) )
+
+/*
+ * Example in docs.
+ */
+//s3db.list()
+//  .then( results => { console.log("results",results); return results[0].get() } ) //Select the first item from the results.
+//  .then( bucket => { console.log("bucket",bucket); return bucket.list() } )
+//  .then( records => console.log("records",records) )
+//  .fail( error => console.error("error",error) )
+//
+///*
+// * Create a new doc and delete it.
+// */
+//users
+//  .save( {testing:'this-new-doc'} )
+//  .then( user => { console.log("user",user); return user; })
+//  .then( user => users.delete(user.id) )
+//  .then( user => { console.log("deleted ",user); return user; })
+//  .fail( error => console.error("error",error) )
+//  
+///*
+// * Example on docs.
+// */
+//users.save({name : 'Richard Cranium'})
+//  .then( user => users.load(user.id))
+//  .then( user => {
+//    user.size = 1234;
+//    user.sex = 'male';
+//    return users.save(user);
+//  })
+//  .then( user => users.delete(user.id) )
+//  .then( user => console.log("done",user) )
+//  .fail( error => console.error(error.stack) )
+//    
