6b96f848d7c1c57c335a029a8ed547b490f23523
Matt Filion <matt.filion@gmail.com>
2017-04-23 11:47:23 -0700

    Added Document.rename to change the name of the underlying document.

--- src/DocumentFactory.js
+++ src/DocumentFactory.js
@@ -22,6 +22,7 @@ const Document = function(document,idPropertyName,collection) {
   document.save       = () => collection.saveDocument(document);
   document.delete     = () => collection.deleteDocument(document.getId());
   document.refresh    = () => collection.getDocument(document.getId());
+  document.rename     = (newName) => collection.copy(document,newName).then( newDocument => collection.deleteDocument(document.getId()).then( () => newDocument ) )
   document.copyTo     = (targetCollection,newId) => targetCollection.copy(document,newId);
 
   return document;
