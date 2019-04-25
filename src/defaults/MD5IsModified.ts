b70c6ea142d3d188e7a87e4b809843fb07255e2f
Matt Filion <matt.filion@gmail.com>
2019-04-24 18:34:23 -0700

    It makes a work now!

--- src/defaults/MD5IsModified.ts
+++ src/defaults/MD5IsModified.ts
@@ -1,6 +1,6 @@
 import * as crypto from 'crypto';
-import { getMetadata } from "../utils/Metadata";
-import { IsModified } from "../exposed";
+import { getMetadata } from '../utils/Metadata';
+import { IsModified } from '../exposed';
 import { BasicObject } from '../db';
 
 /**
@@ -8,17 +8,16 @@ import { BasicObject } from '../db';
  * exists. If they match, modified returns false. If they dont, it returns true.
  */
 export class MD5IsModified implements IsModified {
-
   /**
-   * 
+   *
    * @param documentToCheck The document that will be saved.
    * @param body serialization of the documentToCheck.
    */
   public isModified(documentToCheck: BasicObject, body: string): boolean {
-    const metadata:BasicObject = getMetadata(documentToCheck);
-    if(metadata) {
-      const md5ToCheck:string = MD5IsModified.md5Hash(body);
-      const md5Before:string = metadata.ContentMD5;
+    const metadata: BasicObject | undefined = getMetadata(documentToCheck);
+    if (metadata) {
+      const md5ToCheck: string = MD5IsModified.md5Hash(body);
+      const md5Before: string = metadata.ContentMD5;
       return md5ToCheck !== md5Before;
     } else {
       return true;
@@ -26,10 +25,13 @@ export class MD5IsModified implements IsModified {
   }
 
   /**
-   * 
+   *
    * @param body to create an MD5 for.
    */
   public static md5Hash(body: string): string {
-    return crypto.createHash('md5').update(body).digest('base64');
+    return crypto
+      .createHash('md5')
+      .update(body)
+      .digest('base64');
   }
 }
